761d4b9580d9527805827cb41dd21455294ed294
Neha <nehasable45@gmail.com>
2024-03-15 22:33:52 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import {Route,Routes,BroswerRouter} from "react-router-dom"
+import {Route,Routes,BrowserRouter} from "react-router-dom"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Dashboard from "./pages/Dashboard"
+import SendMoney from "./pages/SendMoney"
 
 function App() {
 
   return (
     <div>
-       <BroswerRouter>
+       <BrowserRouter>
        <Routes>
         <Route path="/signup" element={<SignUp/>}/>
         <Route path="/signin" element={<SignIn/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/sendmoney" element={<SendMoney/>}/>
-     
        </Routes>
-               </BroswerRouter>
+               </BrowserRouter>
     </div>
   )
 }
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+function SignUp() {
+    return (
+        <>
+        
+           <div className='bg-zinc-400 h-screen flex justify-center items-center'>
+    <div className='bg-white h-[500px] w-[350px] flex justify-center border rounded-lg shadow-lg'>
+        <div className='flex flex-col justify-center'>
+        <h2 className='ml-14 text-4xl font-bold '>Sign Up</h2>
+        <label className='ml-2 mb-6 text-gray-500'>Enter your details to create account</label>
+            <label className='mb-2'>First Name</label><input type='text' className='mb-2 border border-gray-300'></input>
+            <label className='mb-2'>Last Name</label><input type='text' className='mb-2 border border-gray-300'></input>
+            <label className='mb-2'>Username</label><input type='text' className='mb-2 border border-gray-300'></input>
+            <label className='mb-2'>Password</label><input type='text' className='mb-2 border border-gray-300'></input>
+            <button className='bg-black text-white mt-3 mb-3 border rounded-lg border-gray-300 ' >Sign Up</button>
+            <label >Already have an account? <Link to="/signin"><button className='underline'>Sign in</button></Link></label>
+        </div>
+       
+    </div>
+</div>
+
+        </>
+    );
+}
+
+export default SignUp;
