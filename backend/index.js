dd29795aa39c0186f13c9409c87ee331c7fa8598
Neha <nehasable45@gmail.com>
2024-03-19 18:28:44 +0530

    backend commit

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,18 @@ const cors=require("cors")
 const mainRouter=require("./routes/index")
 const app= express()
 const jwt=require("jsonwebtoken")
+require('dotenv').config();
+
+const port = process.env.PORT ;
+const dbUrl = process.env.DB_URL;
 
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb+srv://nehasable45:Neha12345@cluster0.z6oczab.mongodb.net/wallet")
+mongoose.connect(dbUrl)
 app.use("/api/v1",mainRouter)  //intializes use of Router with url
 
-app.listen("3000",function(err){
+app.listen(port,function(err){
     if(err)console.log(err)
     console.log("Server is running on 3000")
 })
